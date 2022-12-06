5dd1839e1bb3cb38fe531f6d1d7b4e5bfa6eea56
luke <luke.rast@gmail.com>
2022-12-06 12:30:44 -0700

    fixed config

--- envSetup.sh
+++
@@ -1,9 +0,0 @@
-# script for setting up the server environment
-
-export FLASK_APP=run
-export FLASK_ENV=development
-export FLASK_DEBUG=True
-
-export FLASK_SECRET_KEY='TEMPTEMPTEMP'
-
-export FLASK_DATABASE_URL='/Users/luke/Documents/tools/organizer/webapp/database/pages.db'
